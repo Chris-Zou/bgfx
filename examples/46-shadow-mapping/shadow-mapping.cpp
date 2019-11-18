7f4883f9bc8eb7836ab6a8c625f06acb569b4d46
Chris-Zou <zou_pp@outlook.com>
2019-11-18 21:57:02 +0800

    shadow mapping -- phase 1

--- examples/46-shadow-mapping/shadow-mapping.cpp
+++ examples/46-shadow-mapping/shadow-mapping.cpp
@@ -70,8 +70,48 @@ namespace CSM
 		bgfx::UniformHandle u_lightViewProj = BGFX_INVALID_HANDLE;
 		bgfx::UniformHandle u_samplingDisk = BGFX_INVALID_HANDLE;
 		bgfx::UniformHandle u_cascadeBounds = BGFX_INVALID_HANDLE;
-		bgfx::UniformHandle u_shadowMaps[NUM_CASCADES] = { BGFX_INVALID_HANDLE };
+		bgfx::UniformHandle s_shadowMaps[NUM_CASCADES] = { BGFX_INVALID_HANDLE };
 	};
+
+	void init(DirectionalLight& light)
+	{
+		light.m_cascadeBounds[0].w = 0.035f;
+
+		light.u_directionalLightParams = bgfx::createUniform("u_directionalLightParams", bgfx::UniformType::Vec4, 2);
+		light.u_lightViewProj = bgfx::createUniform("u_lightViewProj", bgfx::UniformType::Mat4, NUM_CASCADES);
+		light.u_samplingDisk = bgfx::createUniform("u_samplingDisk", bgfx::UniformType::Vec4, 4u);
+		light.u_cascadeBounds = bgfx::createUniform("u_cascadeBounds", bgfx::UniformType::Vec4, numDepthUniforms);
+
+		for (size_t i = 0; i < NUM_CASCADES; ++i)
+		{
+			char name[] = "s_shadowMap_x";;
+			bx::toString(&name[12], 2, i);
+			light.s_shadowMaps[i] = bgfx::createUniform(name, bgfx::UniformType::Sampler);
+		}
+	}
+
+	void destroy(DirectionalLight& light)
+	{
+		bgfx::destroy(light.u_directionalLightParams);
+		bgfx::destroy(light.u_lightViewProj);
+		bgfx::destroy(light.u_samplingDisk);
+		bgfx::destroy(light.u_cascadeBounds);
+		for (bgfx::UniformHandle shadowMap : light.s_shadowMaps)
+		{
+			bgfx::destroy(shadowMap);
+		}
+	}
+
+	void bindUniforms(const DirectionalLight& light, const bgfx::TextureHandle shadowMapTextures[NUM_CASCADES]) {
+		bgfx::setUniform(light.u_directionalLightParams, &light, 2);
+		bgfx::setUniform(light.u_lightViewProj, glm::value_ptr(light.m_cascadeTransforms[0]), NUM_CASCADES);
+		bgfx::setUniform(light.u_samplingDisk, glm::value_ptr(poissonPattern[0]), 8u);
+		bgfx::setUniform(light.u_cascadeBounds, light.m_cascadeBounds, NUM_CASCADES);
+		for (uint8_t i = 0; i < NUM_CASCADES; i++)
+		{
+			bgfx::setTexture(i + 5, light.s_shadowMaps[i], shadowMapTextures[i], BGFX_SAMPLER_UVW_CLAMP);
+		}
+	}
 
 	struct PBRShaderUniforms
 	{
@@ -151,6 +191,13 @@ namespace CSM
 		bgfx::destroy(uniforms.s_randomTexture);
 	}
 
+	void bindUniforms(const SceneUniforms& uniforms, const bx::Vec3 cameraPos)
+	{
+		bgfx::setUniform(uniforms.m_shadowMapParams, &uniforms.m_manualBias);
+		bgfx::setUniform(uniforms.m_cameraPos, &cameraPos);
+		bgfx::setTexture(9, uniforms.s_randomTexture, uniforms.m_randomTexture);
+	}
+
 	struct DepthReductionUniforms
 	{
 		bgfx::UniformHandle u_params = BGFX_INVALID_HANDLE;
@@ -204,7 +251,6 @@ namespace CSM
 			init.resolution.reset = m_reset;
 			bgfx::init(init);
 
-			// Enable m_debug text.
 			bgfx::setDebug(m_debug);
 
 			bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH | BGFX_CLEAR_STENCIL, 0x000000ff, 1.0f, 0);
@@ -217,19 +263,172 @@ namespace CSM
 				return;
 			}
 
-			
+			m_directionalShadowMapProgram = loadProgram("vs_directional_shadowmap", "fs_directional_shadowmap");
+			m_prepassProgram = loadProgram("vs_z_prepass", "fs_z_prepass");
+			m_pbrShader = loadProgram("vs_shadowed_mesh", "fs_shadowed_mesh");
+			m_pbrShaderWithMasking = loadProgram("vs_shadowed_mesh", "fs_shadowed_mesh_masked");
+			m_depthReductionInitial = loadProgram("cs_depth_reduction_initial", nullptr);
+			m_depthReductionGeneral = loadProgram("cs_depth_reduction_general", nullptr);
+			m_drawDepthDebugProgram = loadProgram("vs_texture_pass_through", "fs_texture_pass_through");
+
+			m_model = Dolphin::loadGltfModel("meshes/Sponza/", "Sponza.gltf");
+
+			CSM::init(m_pbrUniforms);
+			CSM::init(m_sceneUniforms);
+			CSM::init(m_directionalLight);
+			CSM::init(m_depthReductionUniforms);
+
+			m_shadowMapDebugSampler = bgfx::createUniform("s_input", bgfx::UniformType::Sampler);
+			m_sceneUniforms.m_randomTexture = loadTexture("textures/random.png");
+
+			m_toneMapParams.m_width = m_width;
+			m_toneMapParams.m_height = m_height;
+			m_toneMapParams.m_originBottomLeft = m_caps->originBottomLeft;
+			m_toneMapParams.m_minLogLuminance = -5.0f;
+			m_toneMapParams.m_maxLogLuminance = 10.0f;
+
+			for (size_t i = 0; i < NUM_CASCADES; ++i)
+			{
+				bgfx::Attachment attachment;
+				m_shadowMaps[i] = bgfx::createTexture2D(m_shadowMapWidth, m_shadowMapWidth, false, 1, bgfx::TextureFormat::D32);
+				attachment.init(m_shadowMaps[i], bgfx::Access::Write);
+				m_shadowMapFrameBuffers[i] = bgfx::createFrameBuffer(1, &attachment, true);
+			}
+
+			m_cpuReadableDepth = bgfx::createTexture2D(1, 1, false, 1, bgfx::TextureFormat::RG16F, BGFX_TEXTURE_BLIT_DST | BGFX_TEXTURE_READ_BACK);
+
+			imguiCreate();
+
+			s_texelHalf = bgfx::RendererType::Direct3D9 == m_caps->rendererType ? 0.5f : 0.0f;
+
+			cameraCreate();
+			cameraSetPosition({ 0.0f, 2.0f, 0.0f });
+
+			m_oldWidth = 0;
+			m_oldHeight = 0;
+			m_oldReset = m_reset;
+
+			m_time = 0.0f;
 		}
 
 		virtual int shutdown() override
 		{
-			
+			if (!m_isComputeSupported)
+			{
+				return 0;
+			}
+
+			if (bgfx::isValid(m_pbrFrameBuffer))
+			{
+				bgfx::destroy(m_pbrFrameBuffer);
+			}
+
+			if (bgfx::isValid(m_shadowMapFrameBuffers[0]))
+			{
+				for (size_t i = 0; i < NUM_CASCADES; ++i) {
+					bgfx::destroy(m_shadowMapFrameBuffers[i]);
+				}
+				for (auto texture : m_depthReductionTargets) {
+					bgfx::destroy(texture);
+				}
+				bgfx::destroy(m_cpuReadableDepth);
+			}
+
+			m_toneMapPass.destroy();
+
+			destroy(m_directionalLight);
+			destroy(m_depthReductionUniforms);
+			destroy(m_pbrUniforms);
+			destroy(m_sceneUniforms);
+			bgfx::destroy(m_shadowMapDebugSampler);
+			Dolphin::destroy(m_model);
+			bgfx::destroy(m_drawDepthDebugProgram);
+			bgfx::destroy(m_depthReductionGeneral);
+			bgfx::destroy(m_depthReductionInitial);
+			bgfx::destroy(m_directionalShadowMapProgram);
+			bgfx::destroy(m_prepassProgram);
+			bgfx::destroy(m_pbrShader);
+			bgfx::destroy(m_pbrShaderWithMasking);
+
+			cameraDestroy();
+
+			imguiDestroy();
+
+			bgfx::shutdown();
 
 			return 0;
 		}
 
+		void renderMeshes(const Dolphin::MeshGroup& meshes, const bx::Vec3& cameraPos, const uint64_t state, const bgfx::ProgramHandle program, const bgfx::ViewId viewId) const
+		{
+			for (size_t i = 0; i < meshes.meshes.size(); ++i)
+			{
+				const auto&  mesh = meshes.meshes[i];
+				const auto& transform = meshes.transforms[i];
+				const auto& material = meshes.materials[i];
+
+				bgfx::setState(state);
+				bindUniforms(m_pbrUniforms, material, transform);
+				bindUniforms(m_sceneUniforms, cameraPos);
+				bindUniforms(m_directionalLight, m_shadowMaps);
+
+				mesh.setBuffers();
+
+				bgfx::submit(viewId, program);
+			}
+		}
+
+		uint16_t getDispatchSize(uint16_t dim, uint16_t threadCount) const
+		{
+			uint16_t dispatchSize = dim / threadCount;
+			dispatchSize += dim % threadCount > 0 ? 1 : 0;
+			return dispatchSize;
+		}
+
+		void setupDepthReductionTargets(uint16_t width, uint16_t height)
+		{
+			for (bgfx::TextureHandle texture : m_depthReductionTargets)
+			{
+				bgfx::destroy(texture);
+			}
+
+			m_depthReductionTargets.clear();
+
+			while (width > 1 || height > 1)
+			{
+				width = getDispatchSize(width, THREAD_COUNT_PER_DIM);
+				height = getDispatchSize(height, THREAD_COUNT_PER_DIM);
+				bgfx::TextureHandle texture = bgfx::createTexture2D(width, height, false, 0, bgfx::TextureFormat::RG16F, BGFX_TEXTURE_COMPUTE_WRITE);
+				m_depthReductionTargets.push_back(texture);
+			}
+		}
+
 		bool update() override
 		{
-			
+			if (entry::processEvents(m_width, m_height, m_debug, m_reset, &m_mouseState))
+			{
+				return false;
+			}
+
+			if (!m_isComputeSupported)
+				return false;
+
+			if (!bgfx::isValid(m_pbrFrameBuffer) || m_oldWidth != m_width || m_oldHeight != m_height || m_oldReset != m_reset)
+			{
+				m_oldWidth = m_width;
+				m_oldHeight = m_height;
+				m_oldReset = m_reset;
+
+				uint32_t msaa = (m_reset & BGFX_RESET_MSAA_MASK) >> BGFX_RESET_MSAA_SHIFT;
+
+				if (bgfx::isValid(m_pbrFrameBuffer))
+				{
+					bgfx::destroy(m_pbrFrameBuffer);
+				}
+
+				m_toneMapParams.m_width = m_width;
+				m_toneMapParams.m_height = m_height;
+			}
 		}
 
 	public:
@@ -263,6 +462,10 @@ namespace CSM
 
 		std::vector < bgfx::TextureHandle> m_depthReductionTargets;
 		bgfx::TextureHandle m_cpuReadableDepth;
+
+		PBRShaderUniforms m_pbrUniforms = {};
+		SceneUniforms m_sceneUniforms = {};
+		DepthReductionUniforms m_depthReductionUniforms = {};
 
 		bgfx::UniformHandle m_shadowMapDebugSampler;
 		Dolphin::Model m_model;
